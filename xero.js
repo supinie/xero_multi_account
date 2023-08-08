2d1869ac09698adf37ff4210b5cd56bcd75ff51a
supinie <86788874+supinie@users.noreply.github.com>
2023-08-08 10:37:09 +0100

    trying different api

---
+++ xero.js
@@ -0,0 +1,25 @@
+// from_url = window.location.href;
+// short_code = from_url.split("=")[1];
+
+// window.location = "ext+container:name=" + short_code + "&url=" + from_url;
+
+let pattern = "https://developer.mozilla.org/*";
+const targetUrl =
+  "https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Your_second_WebExtension/frog.jpg";
+
+function redirect(requestDetails) {
+  console.log(`Redirecting: ${requestDetails.url}`);
+  if (requestDetails.url === targetUrl) {
+    return;
+  }
+  return {
+    redirectUrl: targetUrl,
+  };
+}
+
+browser.webRequest.onBeforeRequest.addListener(
+  redirect,
+  { urls: [pattern], types: ["image"] },
+  ["blocking"],
+);
+
